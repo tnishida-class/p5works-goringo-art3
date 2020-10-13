600a6266900d8f9b55fec802726667b2c36b449a
goringo-art3 <72755581+goringo-art3@users.noreply.github.com>
2020-10-13 11:52:38 +0900

    Update 1-1 again
    Co-Authored-By: Takeshi Nishida <takeshi-nishida@users.noreply.github.com>

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -4,12 +4,12 @@ function setup() {
 
 function draw() {
   background(0);
-  strokeWeight(5);
+  strokeWeight(4);
   fill(178, 100, 162);
   triangle(12,120,116,120,116,10);
   fill(94, 185, 84);
   triangle(0,108,106,108,106,0);
-  fill(255);
+  fill(133);
   textSize(32);
   textFont("serif");
   text("46", 68, 100);
